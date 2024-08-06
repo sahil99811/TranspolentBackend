6e995ac9c27cbeb8d6722bde21738a3f32ac0758
sahil99811 <sp220253@gmail.com>
2024-08-06 23:23:48 +0530

    fix issue

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 const PORT = 5000;
 app.use(express.json())
 app.use(scrapeRoutes);
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://transpolentfrontend.vercel.app'); // Replace with your frontend URL
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cors({
   origin: "http://localhost:3000"
 }));
